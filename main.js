9fbf7a00c0dc5972961e01f8d6dbb6e8238b21ce
cond1t <qonitimani@gmail.com>
2025-02-22 22:01:03 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,3 @@
-
-onload = () => {
-    const c = setTimeout(() => {
-      document.body.classList.remove("not-loaded");
-  
-      const titles = ('semangat buat vidionya inezz!!').split('')
-      const titleElement = document.getElementById('title');
-      let index = 0;
-  
-      function appendTitle() {
-        if (index < titles.length) {
-          titleElement.innerHTML += titles[index];
-          index++;
-          setTimeout(appendTitle, 150); // 1000ms delay
-        }
-      }
-  
-      appendTitle();
-  
-      clearTimeout(c);
-    }, 1000);
-  };
+onload = () =>{
+        document.body.classList.remove("container");
+};
